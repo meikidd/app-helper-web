dcb0f68857415d36700b8c0099f3b1922cca56aa
meikidd <meikidd@gmail.com>
2016-09-25 16:21:45 +0800

    update: add koa-static

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const koa = require('koa');
-const views = require('koa-views');
 const path = require('path');
+const views = require('koa-views');
+const htdocs = require('koa-static');
 const router = require('./app/router');
 
 var app = new koa();
+
+// static files
+app.use(htdocs('./htdocs'));
 
 // views
 app.use(views(path.resolve(__dirname, 'app/view'), { extension: 'ejs' }));
